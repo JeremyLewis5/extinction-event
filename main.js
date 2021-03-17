5a59127cf45a3a5669a76c65964b3bb2ea826d40
JeremyLewis5 <jeremy.lewis@codeimmersives.com>
2021-03-17 16:41:49 -0500

    new

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
-const linedOut = document.querySelector('.ordered');
+const linedOut = document.querySelectorAll('ol');
 
 for (const line of linedOut) {
-linedOut.addEventListener("click", function (event) {
+    line.addEventListener("click", function (event) {
     
     const elementThatWasClicked = event.target;
     elementThatWasClicked.style.textDecoration = 'line-through';
 });
 }
+
+const fadeAway = document.querySelectorAll('ul');
+
+for (fade of fadeAway) {
+    fade.addEventListener('click', function (event) {
+
+        const elementThatWasClicked = event.target;
+        elementThatWasClicked.style.opacity = '0';
+
+    });
+}
+
+const picFade = document.querySelectorAll('.feature');
+
+for (pic of picFade) {
+    pic.addEventListener('click', function (event) {
+    
+    const elementThatWasClicked = event.target;
+    elementThatWasClicked.style.opacity = '0';   
+    });
+}
+
+//const button = document.querySelector("#destroy-all");
+
+
+
