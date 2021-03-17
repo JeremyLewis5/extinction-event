636553fecb9d878962feaa3c5c06a94d51661c29
JeremyLewis5 <jeremy.lewis@codeimmersives.com>
2021-03-17 14:38:29 -0500

    new

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const linedOut = document.querySelector('.ordered');
-linedOut.addEventListener("click", function (event) {
-    const clickedElement = event.target;
+linedOut.addEventListener("click", function () {
+    
     clickedElement.style.strike();
 })
